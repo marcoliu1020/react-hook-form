1d677344442c839211cbc51a087fddb589a1dc9d
Marco <a0931900802@gmail.com>
2024-07-07 23:15:46 +0800

    add react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
-
-  return (
-    <>
-        
-    </>
-  )
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  age: number;
 }
 
-export default App
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IFormInput>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  console.log("errors: ", errors);
+
+  console.log("watch: ", watch());
+  // {firstName: '12342', lastName: '', age: ''}
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <label htmlFor="firstName">firstName</label>
+        <input
+          {...register("firstName", {
+            required: `this is required`,
+            minLength: {
+              value: 4,
+              message: "too short",
+            },
+          })}
+        />
+        <p style={{ color: "red" }}>{errors.firstName?.message}</p>
+      </fieldset>
+
+      <fieldset>
+        <label htmlFor="lastName">lastName</label>
+        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        <p style={{ color: "red" }}>{errors.lastName?.message}</p>
+      </fieldset>
+
+      <fieldset>
+        <label htmlFor="age">age</label>l
+        <input type="number" {...register("age", { min: 18, max: 99 })} />
+      </fieldset>
+
+      <input type="submit" />
+    </form>
+  );
+}
